feb5df00ebc328030229f65df90d0cda3195312f
Ian Sirma <ikipsorno@gmail.com>
2023-06-15 13:57:41 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,19 @@
+#include <stdio.h>
 #include <unistd.h>
 
-int main(void) {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
+/**
+
+* main - Entry point
+
+*
+
+* Return: Always 1 (Success)
+
+*/
+
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
     return 1;
 }
 
